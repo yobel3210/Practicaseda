dda094b62e4e4a09fb5ebcc0681306b06a23de49
yobel3210 <doloreszeferinoyobel@gmail.com>
2024-05-03 18:04:21 -0600

    Modificación al programa change.py para que entregue la menor cantidad de monedas posibles usando sólo las monedas disponibles.

--- change.py
+++ change.py
@@ -7,29 +7,29 @@
 # ## #############################################################
 
 import sys
-coins = [0.1, 0.2, 0.5, 1, 2, 5, 10]
-# coins = {
-# 	0.1 : 3,
-# 	0.2 : 3,
-# 	0.5 : 2,
-# 	  1 : 0,
-# 	  2 : 3,
-# 	  5 : 0,
-# 	 10 : 3,
-# 	 20 : 3,
-# 	 50 : 3,
-# 	100 : 3,
-# }
+coins = {
+ 	0.1 : 3,
+ 	0.2 : 3,
+ 	0.5 : 2,
+ 	  1 : 0,
+ 	  2 : 3,
+ 	  5 : 0,
+ 	 10 : 3,
+ 	 20 : 3,
+ 	 50 : 3,
+ 	100 : 3,
+ }
 
 def change(amount):
-	i = len(coins) -1
-	my_change = [[coin, 0] for coin in coins]
-	while(amount > 0 and i >= 0):
-		if amount >= coins[i]:
-			amount-= coins[i]
-			my_change[i][1]+=1
-		else:
-			i-= 1
+	coin_keys = sorted(coins.keys(), reverse=True)
+	my_change = {coin: 0 for coin in coins}
+	for coin in coin_keys:
+		while amount >= coin and coins[coin]>0:
+			amount -= coin
+			my_change[coin] += 1
+			coins[coin] -= 1
+		if amount ==0:
+			break
 	return my_change
 # end def
 
@@ -37,7 +37,7 @@ def change(amount):
 def main(argv):
 	amount = float(argv[1])
 	print("Cambio de", amount, "en:")
-	for (coin, num) in sorted(change(amount), reverse=True):
+	for (coin, num) in sorted(change(amount).items(), reverse=True):
 		if not num:
 			continue
 		print(num,
